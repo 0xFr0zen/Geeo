14d5c3cbb35aba6e9d7850959393bf6b53a2fd7d
0xFr0zen <0xfr0zen@gmail.com>
2019-12-18 10:20:32 +0100

    Gitignore, 'output'-folder removed.
    Updated Gitignore once again, seems not to ignore the output folder. So I deleted it, and will try if it ignores it or not.

--- output/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Database_1 = require("./dev/Database");
-var dbuser = new Database_1.DatabaseUser('geeo_admin_penetrator');
-console.log(dbuser);
