0a5488a1becbe94d94fca36350840e09862b65f3
0xFr0zen <0xfr0zen@gmail.com>
2019-12-25 00:55:54 +0100

    Update init.ts

--- init.ts
+++ init.ts
@@ -3,7 +3,6 @@ import User from './dev/User/index';
 // Device.hasIdentity('none'); // initalizes
 let u = User.from(Device.createIdentity('oezguer'));
 console.log(u);
-
 console.log(u.getCreated());
 // let s = {"key":"s"};
 // let s2 = {"key":1,"key2":"s"};
