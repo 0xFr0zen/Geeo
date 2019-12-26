7fe7f73aafca606dfeec000b5b2da2006e98534e
0xFr0zen <0xfr0zen@gmail.com>
2019-12-27 00:33:43 +0100

    Update init.ts

--- init.ts
+++ init.ts
@@ -1,7 +1,7 @@
 import Device from './dev/Device/index';
 import User from './dev/User/index';
 import decryptText from './dev/Crypt/decrypt';
-let i = Device.createIdentity('oezguer')
+let i = Device.createIdentity('oezguer');
 // let text = "";
 // let j = JSON.parse(decryptText(text, Device.getPublicKey(), Device.getIdentity('oezguer').getPrivateKey()));
 // console.log();
