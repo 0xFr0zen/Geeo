e97c58463369bcc9e6eb2a1ff1382054434fc645
0xFr0zen <0xfr0zen@gmail.com>
2020-01-05 23:35:52 +0100

    System
    removed the "hello" call

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import System from './dev/system/index';
 
 
 (async ()=>{
-    console.log("hello");
     let system:System = new System();
     system.on('ready', () => {
         console.log("System running.");
