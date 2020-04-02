2161f51e048e663d26e9d0dac9f77f263faf9801
0xFr0zen <0xfr0zen@gmail.com>
2020-04-02 12:24:30 +0200

    updates

--- dev/system/Web/Scripts/essentials.js
+++ dev/system/Web/Scripts/essentials.js
@@ -1,54 +1,91 @@
-$(document).ready(function() {
-    $('#main').css('opacity', '1');
-    $('#menu #mode').on('click', e => {
-        let modes = {
-            dark: { goto: 'light', text: 'brightness_2' },
-            light: { goto: 'dark', text: 'wb_sunny' },
-        };
-        let elem = $(e.target);
-        var currentState = elem.attr('state');
-        elem.attr('state', modes[currentState].goto);
-
-        if (window.localStorage) {
-            localStorage.setItem('brightnessmode', modes[currentState].goto);
-        }
-        elem.text(modes[currentState].text);
-        document.body.toggleAttribute('dark');
-    });
-    $('#switcher').on('click', () => {
-        let laststate = $('#switcher').attr('state');
-        if (laststate === 'cashier') {
-            $('#switcher').attr('state', 'overview');
-        } else {
-            $('#switcher').attr('state', 'cashier');
-        }
-    });
-    setTimeout(() => {
-        if (window.localStorage) {
-            let br_mode = localStorage.getItem('brightnessmode');
-            if (typeof br_mode === 'undefined' || br_mode == null) {
-                br_mode = 'light';
-                localStorage.setItem('brightnessmode', br_mode);
-            }
-            $('#menu #mode').attr('state', br_mode);
-            let modes = {
-                dark: { goto: 'light', text: 'wb_sunny' },
-                light: { goto: 'dark', text: 'brightness_2' },
-            };
-            $('#menu #mode').text(modes[br_mode].text);
-
-            if (br_mode === 'dark') {
-                document.body.setAttribute('dark', '');
-            } else {
-                document.body.removeAttribute('dark');
-            }
-        }
+window.ResourceLoaderPackage = [];
+$(document).ready(async function() {
+    PageLoader().then(b => {
+        $('#loader').toggleClass('hidden');
+        $('#main').toggleClass('hidden');
         setTimeout(() => {
-            $('#loader').toggleClass('hidden');
-            $('#main').toggleClass('hidden');
-            setTimeout(() => {
-                $('#loader').css('display', 'none');
-            }, 300);
-        }, 500);
-    }, 500);
+            $('#loader').css('display', 'none');
+        }, 300);
+    });
 });
+function PageLoader() {
+    return Promise.all(prepareResourceLoader());
+}
+function ResourceLoader(fn) {
+    return new Promise((resolve, reject) => {
+        if (fn()) {
+            resolve(true);
+        } else {
+            reject('failed');
+        }
+    });
+}
+function prepareResourceLoader() {
+    window.ResourceLoaderPackage.push(
+        ResourceLoader(() => {
+            $('#main').css('opacity', '1');
+            return true;
+        })
+    );
+    window.ResourceLoaderPackage.push(
+        ResourceLoader(() => {
+            $('#menu #mode').on('click', e => {
+                let modes = {
+                    dark: { goto: 'light', text: 'brightness_2' },
+                    light: { goto: 'dark', text: 'wb_sunny' },
+                };
+                let elem = $(e.target);
+                var currentState = elem.attr('state');
+                elem.attr('state', modes[currentState].goto);
+
+                if (window.localStorage) {
+                    localStorage.setItem(
+                        'brightnessmode',
+                        modes[currentState].goto
+                    );
+                }
+                elem.text(modes[currentState].text);
+                document.body.toggleAttribute('dark');
+            });
+            return true;
+        })
+    );
+    window.ResourceLoaderPackage.push(
+        ResourceLoader(() => {
+            $('#switcher').on('click', () => {
+                let laststate = $('#switcher').attr('state');
+                if (laststate === 'cashier') {
+                    $('#switcher').attr('state', 'overview');
+                } else {
+                    $('#switcher').attr('state', 'cashier');
+                }
+            });
+            return true;
+        })
+    );
+    window.ResourceLoaderPackage.push(
+        ResourceLoader(() => {
+            if (window.localStorage) {
+                let br_mode = localStorage.getItem('brightnessmode');
+                if (typeof br_mode === 'undefined' || br_mode == null) {
+                    br_mode = 'light';
+                    localStorage.setItem('brightnessmode', br_mode);
+                }
+                $('#menu #mode').attr('state', br_mode);
+                let modes = {
+                    dark: { goto: 'light', text: 'wb_sunny' },
+                    light: { goto: 'dark', text: 'brightness_2' },
+                };
+                $('#menu #mode').text(modes[br_mode].text);
+
+                if (br_mode === 'dark') {
+                    document.body.setAttribute('dark', '');
+                } else {
+                    document.body.removeAttribute('dark');
+                }
+            }
+            return true;
+        })
+    );
+    return window.ResourceLoaderPackage;
+}
